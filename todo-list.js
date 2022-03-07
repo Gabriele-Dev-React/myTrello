1bc80a613c4c91eeaa151cf3d6b2d32df4ff3a70
Gabriele Culotta <culottagabriele@MacBook-Pro-di-Gabriele.local>
2022-03-07 10:08:24 +0100

    feat(general): define variables

---
+++ todo-list.js
@@ -0,0 +1,31 @@
+//ID RANDOM
+const IdCreator = Math.floor(Math.random() * 900000) + 100000;
+
+//TASK
+const task = {
+  description: "calcio",
+};
+
+// ARRAY DI TASK
+const tasklist = [];
+
+//ASSIGN
+const assignTo = [
+  {
+    id: IdCreator,
+    nome: "filippo",
+    cognome: "rossi",
+  },
+  {
+    id: IdCreator,
+    nome: "Giuseppe",
+    cognome: "Blu",
+  },
+];
+
+// PRIORITY ID
+const priorityID = [
+  { id: 1, desc: "low" },
+  { id: 2, desc: "medium" },
+  { id: 3, desc: "high" },
+];
